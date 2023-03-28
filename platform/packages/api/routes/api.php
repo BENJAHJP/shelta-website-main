72b340b128f20808b01c0b85acc9f85093652ae7
BENJAH <benjahjp@gmail.com>
2023-03-28 21:00:31 +0300

    api

--- platform/packages/api/routes/api.php
+++ platform/packages/api/routes/api.php
@@ -20,5 +20,22 @@ Route::group([
         Route::put('me', 'ProfileController@updateProfile');
         Route::post('update/avatar', 'ProfileController@updateAvatar');
         Route::put('update/password', 'ProfileController@updatePassword');
+
+        Route::get('authenticate', [UserController::class, 'authenticate']);
+        Route::get('products_by_category/{categoryId}',[MobileProductController::class, 'products_by_category']);
+        Route::get('product_details/{productId}',[MobileProductController::class, 'product_details']);
+        Route::get('products_by_name/{name}',[MobileProductController::class, 'products_by_name']);
+        Route::post('search_product/{search_query}',[MobileProductController::class, 'search_product']);
+        Route::post('bookings', [MobileProductController::class, 'store_booking']);
+        Route::post('password', [MobileProductController::class, 'store_password']);
+        Route::post('email', [MobileProductController::class, 'store_email']);
+        Route::get('bookings/{name}', [MobileProductController::class, 'get_bookings']);
+        Route::get('booking_by_id/{id}', [MobileProductController::class, 'get_booking_by_id']);
+        Route::get('top_products',[MobileProductController::class, 'top_products']);
+        Route::post('update_name/{id}',[MobileProductController::class, 'update_name']);
+        Route::post('update_contact/{id}',[MobileProductController::class, 'update_contact']);
+        Route::post('update_email/{id}',[MobileProductController::class, 'update_email']);
+        Route::post('update_password/{id}',[MobileProductController::class, 'update_password']);
+        
     });
 });
---
+++ platform/packages/api/src/Http/Controllers/MobileController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Botble\Api\Http\Controllers;
+use ApiHelper;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class MobileController extends Controller
+{
+    
+}
