2928db7a4c4a51a665ec68a26acbdd384cd4175d
BENJAH <benjahjp@gmail.com>
2023-03-28 21:02:35 +0300

    api fix

--- platform/packages/api/routes/api.php
+++ platform/packages/api/routes/api.php
@@ -5,6 +5,9 @@ Route::group([
     'namespace' => 'Botble\Api\Http\Controllers',
     'middleware' => ['api'],
 ], function () {
+    // Route::post('/login',[UserController::class,'index']);
+    // Route::post('/signup',[UserController::class,'store']);
+
     Route::get('connected', 'MobileController@connected');
 
     Route::post('register', 'AuthenticationController@register');
@@ -21,21 +24,21 @@ Route::group([
         Route::post('update/avatar', 'ProfileController@updateAvatar');
         Route::put('update/password', 'ProfileController@updatePassword');
 
-        Route::get('authenticate', [UserController::class, 'authenticate']);
-        Route::get('products_by_category/{categoryId}',[MobileProductController::class, 'products_by_category']);
-        Route::get('product_details/{productId}',[MobileProductController::class, 'product_details']);
-        Route::get('products_by_name/{name}',[MobileProductController::class, 'products_by_name']);
-        Route::post('search_product/{search_query}',[MobileProductController::class, 'search_product']);
-        Route::post('bookings', [MobileProductController::class, 'store_booking']);
-        Route::post('password', [MobileProductController::class, 'store_password']);
-        Route::post('email', [MobileProductController::class, 'store_email']);
-        Route::get('bookings/{name}', [MobileProductController::class, 'get_bookings']);
-        Route::get('booking_by_id/{id}', [MobileProductController::class, 'get_booking_by_id']);
-        Route::get('top_products',[MobileProductController::class, 'top_products']);
-        Route::post('update_name/{id}',[MobileProductController::class, 'update_name']);
-        Route::post('update_contact/{id}',[MobileProductController::class, 'update_contact']);
-        Route::post('update_email/{id}',[MobileProductController::class, 'update_email']);
-        Route::post('update_password/{id}',[MobileProductController::class, 'update_password']);
-        
+        // Route::get('authenticate', [UserController::class, 'authenticate']);
+        Route::get('products_by_category/{categoryId}',[MobileController::class, 'products_by_category']);
+        Route::get('product_details/{productId}',[MobileController::class, 'product_details']);
+        Route::get('products_by_name/{name}',[MobileController::class, 'products_by_name']);
+        Route::post('search_product/{search_query}',[MobileController::class, 'search_product']);
+        Route::post('bookings', [MobileController::class, 'store_booking']);
+        Route::post('password', [MobileController::class, 'store_password']);
+        Route::post('email', [MobileController::class, 'store_email']);
+        Route::get('bookings/{name}', [MobileController::class, 'get_bookings']);
+        Route::get('booking_by_id/{id}', [MobileController::class, 'get_booking_by_id']);
+        Route::get('top_products',[MobileController::class, 'top_products']);
+        Route::post('update_name/{id}',[MobileController::class, 'update_name']);
+        Route::post('update_contact/{id}',[MobileController::class, 'update_contact']);
+        Route::post('update_email/{id}',[MobileController::class, 'update_email']);
+        Route::post('update_password/{id}',[MobileController::class, 'update_password']);
+
     });
 });
